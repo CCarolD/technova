361955d053e6e3fb3928ae1226b2a12a8ac3e7b3
cindy-cyber <cindyyfy19@gmail.com>
2021-08-28 21:41:56 +0800

    Co-authored-by: Crystal <CrystalZhou0529@users.noreply.github.com>

--- js/table.js
+++ js/table.js
@@ -11,6 +11,35 @@ $(function() {
 const fetchRecords = async() => {
     const res = await fetch('http://localhost:5000/data');
     const data = await res.json();
-    console.log(data);
+    //console.log(data);
     return data;
 }
+
+
+
+let dataset = fetchRecords();
+
+function buildTable(data){
+    var table = document.getElementById("myTable");
+    console.log(data.length);
+    for (var i = 0; i < data.length; i++){
+        var row = `<tr>
+                        <td>${data[i].id}</td>
+                        <td>${data[i].lastName}</td>
+                        <td>${data[i].firstName}</td>
+                        <td>${data[i].studentId}</td>
+                        <td>${data[i].faculty}</td>
+                        <td>'Fully Vaccinated'</td>
+                        <td>${data[i].doseNum}</td>
+                        <td>${data[i].firstDoseManufacturer}</td>
+                        <td>${data[i].firstDoseDate}</td>
+                        <td>${data[i].secondDoseManufacturer}</td>
+                        <td>${data[i].secondDoseDate}</td>
+                  </tr>`;
+        console.log(row);
+        table.innerHTML += row;
+    }
+}
+
+buildTable(dataset);
+
